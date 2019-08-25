29463b7a52c79504c7f9f27a30f75d4363d1b74e
azizmakda <54510596+azizmakda@users.noreply.github.com>
2019-08-25 14:31:05 -0500

    Add files via upload

---
+++ edge.ino
@@ -0,0 +1,57 @@
+#include <SPI.h> 
+#include <nRF24L01.h>  
+#include <RF24.h>
+
+#include <Servo.h>
+
+Servo servoLeft;
+Servo servoRight;
+
+const byte ReceiverAddress[5] = {'R','x','A','D','C'};       //address of the receiver 
+
+RF24 radio(9, 10);         // initialize a Radio variable, this module is connected to pins 9 and 10
+
+char dataToSend[10] = "Hello";      //the command/data you need to send to the receiver 
+bool newData;
+long distance;
+long time;
+
+int input = 1;
+String command2 = "turn";
+int tempNum = 0; //if 0 == comand 1, if 1 == command 2
+
+String start = "start";
+
+void setup()
+{
+
+  Serial.begin(9600);
+  radio.begin();                                                  //begin the radio module     
+  radio.setDataRate( RF24_250KBPS );           
+  radio.setRetries(3,5);                                     
+  radio.openWritingPipe(ReceiverAddress); 
+  //servoLeft.attach(10);      //  change the digital pin
+  //servoRight.attach(9);      //  change the digital pin     
+  
+}
+
+void loop()
+{
+  Serial.print("Sending instruction: ");     
+  Serial.println(input);             
+  bool result = radio.write( &input, sizeof(input) );     
+  //byte results = radio.write( &input, sizeof (input) ); 
+
+ 
+  if (result) 
+  {         
+       Serial.println(" Data sent and an acknowledge is received");     
+  }     
+ 
+ else 
+  {         
+       Serial.println(" transmission is failed");     
+  }   
+
+
+}
---
+++ thing.ino
@@ -0,0 +1,105 @@
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+Servo servoLeft;
+Servo servoRight;
+int trigPin = 5;
+int echoPin = 6;
+const byte MyAddress[5] = {'R','x','A','D','C'};
+//trig = pin 5
+//echo = pin 6
+//right wheel = pin 3
+//left wheel = pin 2
+
+//left wheel forward = 180, right wheel forward = 0
+//left wheel backward = 0, right wheel backward = 180
+
+RF24 radio(9, 10); //pin 9 and 10 for radio
+
+bool newData = false;
+long distance;
+long duration;
+int input;
+
+void setup(){
+  servoLeft.attach(2); 
+  servoRight.attach(3);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  radio.begin();
+  radio.setDataRate(RF24_250KBPS);
+  radio.openReadingPipe(1, MyAddress);
+  //radio.enableAckPayload(); //add later to have two-way communication
+  radio.startListening();
+}
+
+void loop(){
+  if(radio.available()){
+    Serial.println("--------------------------------");
+    radio.read(&input, sizeof(input));
+    checkDistance();
+    int input2 = input;
+    newData = true;
+    Serial.println("Data received!!");
+    
+    Serial.print("COMMAND = ");
+    Serial.println(input2);
+    if(input2 == 1){
+      checkDistance();
+      Serial.print("Distance: ");
+      Serial.println(distance);
+      
+      if(distance >= 10){
+        Serial.println("MOVING FORWARD");
+        forward();
+        delay(50);
+      } else if(distance < 10){
+          Serial.println("TURNING");
+          stopCar();
+          delay(500);
+          reverse();
+          delay(500);
+          turnVehicle();
+          delay(500);
+          
+      }
+      delay(500);
+    }
+  }
+  distance = 1000;
+}
+
+void forward(){
+  servoLeft.write(180);  // move the left motor clockwise
+  servoRight.write(0);   // move the right motor counterclockwise
+}
+
+void stopCar(){
+  servoLeft.write(90);   // stops the left motor
+  servoRight.write(90);  // stops the right motor
+  delay(500);
+}
+
+void turnVehicle(){
+    servoLeft.write(90);
+    servoRight.write(0);    
+  
+}
+
+void reverse(){
+    servoLeft.write(0);
+    servoRight.write(180);    
+}
+
+void checkDistance(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration*0.034/2;
+}
